3b2939fe4ef5d939ca6992120b16407d87cc5e27
Trevor Nelson <trevnel67@gmail.com>
2017-07-06 00:23:35 -0300

    added a scoreboard with the balls that have been selected.

--- app/src/main/java/trevornelson/bingo_watcher/bingoGame.java
+++ app/src/main/java/trevornelson/bingo_watcher/bingoGame.java
@@ -1,83 +1,38 @@
 package trevornelson.bingo_watcher;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.internal.NavigationMenu;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.gordonwong.materialsheetfab.MaterialSheetFab;
-import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
+import io.github.yavski.fabspeeddial.FabSpeedDial;
+import io.github.yavski.fabspeeddial.SimpleMenuListenerAdapter;
 
 public class bingoGame extends AppCompatActivity {
-    private ActionBarDrawerToggle drawerToggle;
-    private DrawerLayout drawerLayout;
-    private MaterialSheetFab materialSheetFab;
-    private int statusBarColor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bingo_game);
 
-        Fab fab = (Fab) findViewById(R.id.floatingActionButton3);
-        View sheetView = findViewById(R.id.fab_sheet);
-        View overlay = findViewById(R.id.overlay);
-        int statusColor = ContextCompat.getColor(bingoGame.this, R.color.background_card);
-        int fabColor = ContextCompat.getColor(bingoGame.this, R.color.theme_accent);
-
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.opendrawer, R.string.closedrawer);
-        drawerLayout.addDrawerListener(drawerToggle);
-
-        // Initialize material sheet FAB
-        materialSheetFab = new MaterialSheetFab<>(fab, sheetView, overlay, statusColor, fabColor);
-        // Set material sheet item click listeners
-
-        // Set material sheet event listener
-        materialSheetFab.setEventListener(new MaterialSheetFabEventListener() {
+        FabSpeedDial fabSpeedDial = (FabSpeedDial) findViewById(R.id.bingo_add_content);
+        fabSpeedDial.setMenuListener(new SimpleMenuListenerAdapter() {
             @Override
-            public void onShowSheet() {
-                // Save current status bar color
-                statusBarColor = getStatusBarColor();
-                // Set darker status bar color to match the dim overlay
-                setStatusBarColor(ContextCompat.getColor(bingoGame.this,R.color.theme_primary_dark2));
-            }
-
-            @Override
-            public void onHideSheet() {
-                // Restore status bar color
-                setStatusBarColor(statusBarColor);
+            public boolean onPrepareMenu(NavigationMenu navigationMenu) {
+                // TODO: Do something with yout menu items, or return false if you don't want to show them
+                return true;
             }
         });
 
-//        findViewById(R.id.fab_sheet_item_recording).setOnClickListener(this);
-    }
+        LinearLayout linearLayout =  (LinearLayout) findViewById(R.id.bingo_cards);
+        TextView valueTV = new TextView(this);
+        valueTV.setText("hallo hallo");
+        valueTV.setId(2);
+        valueTV.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
 
-    private int getStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return getWindow().getStatusBarColor();
-        }
-        return 0;
-    }
-
-    private void setStatusBarColor(int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(color);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (materialSheetFab.isSheetVisible()) {
-            materialSheetFab.hideSheet();
-        } else {
-            super.onBackPressed();
-        }
+        linearLayout.addView(valueTV);
     }
 }
