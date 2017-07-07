44d432763257626fe9ad0d08cf750453a7b73c44
Trevor Nelson <trevnel67@gmail.com>
2017-07-06 23:50:11 -0300

    ocr is working, need to updated it to do what I want.

--- app/src/main/java/trevornelson/bingo_watcher/bingoGame.java
+++ app/src/main/java/trevornelson/bingo_watcher/bingoGame.java
@@ -1,8 +1,13 @@
 package trevornelson.bingo_watcher;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.internal.NavigationMenu;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -11,6 +16,9 @@ import io.github.yavski.fabspeeddial.FabSpeedDial;
 import io.github.yavski.fabspeeddial.SimpleMenuListenerAdapter;
 
 public class bingoGame extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private TextView valueTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +28,14 @@ public class bingoGame extends AppCompatActivity {
         fabSpeedDial.setMenuListener(new SimpleMenuListenerAdapter() {
             @Override
             public boolean onPrepareMenu(NavigationMenu navigationMenu) {
+                dispatchTakePictureIntent();
                 // TODO: Do something with yout menu items, or return false if you don't want to show them
-                return true;
+                return false;
             }
         });
 
         LinearLayout linearLayout =  (LinearLayout) findViewById(R.id.bingo_cards);
-        TextView valueTV = new TextView(this);
+        valueTV = new TextView(this);
         valueTV.setText("hallo hallo");
         valueTV.setId(2);
         valueTV.setLayoutParams(new LinearLayout.LayoutParams(
@@ -34,5 +43,30 @@ public class bingoGame extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT));
 
         linearLayout.addView(valueTV);
+    }
+
+    public void dispatchTakePictureIntent() {
+        Log.d("TEST", "You hit the button");
+        Intent takePictureIntent = new Intent(this, OcrCaptureActivity.class);
+        takePictureIntent.putExtra(OcrCaptureActivity.AutoFocus, true);
+
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (data != null) {
+                String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
+//                valueTV.setText(R.string.ocr_success);
+                valueTV.setText(text);
+//                Log.d(TAG, "Text read: " + text);
+            } else {
+                valueTV.setText("failed ocr");
+//                Log.d(TAG, "No Text captured, intent data is null");
+            }
+        }
     }
 }
